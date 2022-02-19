00eccdd98f765684953b76abb63326360cd35ff8
Marcelo <ariasmarcelof@gmail.com>
2022-02-19 12:58:53 -0300

    ...

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -8,7 +8,6 @@ const currentFilters = {
 
 const Filters = ({ handleApplyFilters }) => {
   const [filters, setFilters] = useState(currentFilters);
-  const [f, setF] = useState(currentFilters);
 
   const handleUpdateFilters = (field, value) => {
     const current = { ...filters };
